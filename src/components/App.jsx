e03ac1c801e9c04208e977c853bc18f8e2757ab3
Oleksandr <110254286+OleksandrUA777@users.noreply.github.com>
2023-06-14 21:17:59 +0200

    фвв

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { ColorPicker } from './ColorPicker/ColorPicker';
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3*' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91663' },
+  { label: 'Endigo', color: '#3F5185' },
+];
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <ColorPicker options={colorPickerOptions} />;
 };
---
+++ src/components/ColorPicker/ColorPicker.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import './ColorPicker.css';
+
+export const ColorPicker = ({ options }) => {
+  const [activeOptionIdx, setActiveOptionIdx] = useState(0);
+
+  const makeOptionClassName = index => {
+    const optionClasses = ['ColorPicker__option'];
+
+    if (activeOptionIdx === index) {
+      optionClasses.push('ColorPicker_option--active');
+    }
+    return optionClasses.join(' ');
+  };
+
+  const activeOptionLabel = options[activeOptionIdx].label;
+
+  return (
+    <div className="ColorPicker">
+      <h2 className="ColorPicker__title">Color Picker</h2>
+      <div>
+        {<p>Color: {activeOptionLabel}</p>}
+        {options.map(({ label, color }, index) => {
+          return (
+            <button
+              key={label}
+              className={makeOptionClassName(index)}
+              style={{ backgroundColor: color }}
+              onClick={setActiveOptionIdx(index)}
+            ></button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
